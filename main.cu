010e8151105c8a4fdc93b696da273cdd8d92e574
JGMorgan <josegm11194@gmail.com>
2015-11-19 20:52:38 -0500

    updated main
    adjacency matrix is generated and printed

--- main.cu
+++ main.cu
@@ -1,7 +1,27 @@
-typedef struct Node{
-   int key;
-   struct Node** node;
-}Node;
+#include <stdio.h>
+#include <stdlib.h>
 
-int** adjMatrix;
+void printAdjMatrix(int count, int* adjMatrix);
 
+int main(){
+   int* adjMatrix;
+   int count = 10;
+   adjMatrix = (int *)malloc(sizeof(int));
+   int i;
+   for (i = 0; i < count * count; i++){
+      adjMatrix[i] = rand() % 2;      
+   }
+   printAdjMatrix(count, adjMatrix);
+   return 0;
+}
+
+void printAdjMatrix(int count, int* adjMatrix){
+   int i;
+   for (i = 0; i < count; i++){
+      int j;
+      for (j = 0; j < count; j++){
+         printf("%i  ", adjMatrix[(i * count) + j]);
+      } 
+      printf("\n");
+   }
+}
