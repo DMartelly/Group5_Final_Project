93cc28a0b1398ad6c7ddbbe794dd0ef15407b153
Dmartelly <dmart120@fiu.edu>
2016-04-06 19:19:14 -0400

    Major Update
    added -s parameter for the program to show traversed paths
    added plenty of new comments in the code
    clean up and reorgaization

--- main.cu
+++ main.cu
@@ -1,107 +1,150 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/time.h>
 #include <curand.h>
 #include <unistd.h>
 #include <curand_kernel.h>
 
-int* generateAdjMatrix(int count, int* adjMatrix);
-void printAdjMatrix(int count, int* adjMatrix);
-int* multiplyMatrix(int* in,int* in2, int num,int count);
-void CPUMatrixMultiplication(int count, int path, int* matrix);
+const int NUMTHREADS = 1024;
 
-//this function needs to be renamed since it does not do matrix multiplication
-//it just does cuda mem stuff to prep for matrix multiplication
-void GPUMatrixMultiplication(int count, int path, int* matrix, int start, int end);
+/*
+ * Each matrix is one dimentional, but is treated 2 dimentionaly e.g.
+ *
+ * 1 0 1
+ * 0 1 0
+ * 0 1 1
+ *
+ * EQUALS
+ *
+ * {1, 0, 1, 0, 1, 0, 0, 1, 1} as an int
+ *
+ */
 
-#define NUMTHREADS 1024;
-int fTime = 0;
+//Flags
+int fTimeOnly = 0;	//Only print out the computation times
+int fGPUOnly = 0;	//Only preform operation on the GPU
+int fShowPaths = 0; //List all the generated paths
+//------------------------------------------------------------------------------------------
 
+/*
+ * generateAdjMatrix - Returns a new random adjacency matrix
+ *	count - the size of the matrix. the size is count X count)
+ */
+long* generateAdjMatrix(int count) {
+	long* randomMatrix = (long*)malloc(count*count*sizeof(long));
+	int i, j;
 
-//This is the main function
-int main(int argc, char* argv[]){
-	int count = 10;
-	int path = 2;
-	int* adjMatrix = NULL;
-	int gpuOnly = 0;
+	//Set the random seed to the current time
+	srand(time(NULL));
 
-	//start and end of the path
-	int start = 0;
-	int end = 3;
-	
-	//If there is more than 2 parameters
-	opterr = 0;
-	int c;
-	if(argc==1) {
-		fprintf(stderr,"Usage:\n-t: print time only\n-d: default count to 10, path to 2\n-g: preform calculations on GPU only\n-c <num of nodes>\n-p <num of paths>\n-a <start node>\n-b <end node>\n");
-		return 1;
-	}
-
-	while((c = getopt (argc, argv, "dgtc:p:a:b:")) != -1){
-		switch (c)
-		{
-			case 'd':
-				count = 10;
-				path = 2;
-				break;
-			case 'g':
-				gpuOnly = 1;
-				break;
-			case 't':
-				fTime = 1;
-				break;
-			case 'c':
-				count = atoi(optarg);
-				break;
-			case 'p':
-				path = atoi(optarg);
-				break;
-			case 'a':
-                                start = atoi(optarg);
-                                break;
-                        case 'b':
-                                end = atoi(optarg);
-                                break;
-			case '?':
-				if (optopt == 'c' || optopt == 'p'){
-					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-				}
-				else{
-					fprintf(stderr,"Usage:\n-t: print time only\n-d: default count to 10, path to 2\n-g: preform calculations on GPU only\n-c <num of nodes>\n-p <num of paths>\n-a <start node>\n-b <end node>\n");
-				}
-				return 1;
-			default:
-				return 2;
+	//Create a random adjacency matrix using rand. Nodes do not connect to them selves 
+	for (i = 0; i < count; i++){
+		for(j = 0; j < count; j++){
+			if(i != j){
+				long randomResult = rand() % 2;
+				randomMatrix[(i *count) + j] = randomResult;
+				randomMatrix[(j *count) + i] = randomResult;
+			}
 		}
-	
 	}
-	path--;
-
-	//adjMatrix now equals a new Random adjancency  Matrix
-	adjMatrix = generateAdjMatrix(count, adjMatrix);
-
-	//Print the generated adjancency matrix
-	if (!fTime){
-		printf("Generated Adjancency Matritx:\n");
-		printAdjMatrix(count, adjMatrix);
-		printf("\n");
-	}
-
-	//Compute the CPU function
-	if(!gpuOnly) CPUMatrixMultiplication(count, path, adjMatrix);
-	
-	//starting Node is 0 and ending node is 3
-	//temporary test please add commandline args for start and end
-	//Compute the GPU function
-	GPUMatrixMultiplication(count, path, adjMatrix, start, end);	
-	return 0;
+	return randomMatrix;
 }
 
-//takes in a matrix and returns all paths as an int array
-//the int array paths should be a 2d array but for ease of use with cuda it is
-//an linear array just like the matrix
-__global__ void traverse(int* matrix, int* paths, int count, int start, int end, int length, int numPaths){
-	int element = blockIdx.x*blockDim.x + threadIdx.x;
+/*
+ * printAdjMatrix - Prints and adjacency matrix
+ *	count - the height of the matrix
+ *	matrix - the adjacency matrix
+ */
+void printAdjMatrix(int count, long* matrix){
+	int i;
+	for (i = 0; i < count; i++) {
+		int j;
+		for (j = 0; j < count; j++) {
+			printf("%3ld ", matrix[(i * count) + j]);
+		}
+		printf("\n");
+	}
+}
+
+/*
+ * CPUMultiplyMatrix - copyies the cross multiplied matrix of matrixies 1 and 2, into matrix2(The matrixies must be of the same height and width)
+ * The runtime is O(n^3) * t where n = the height of the matrix, t = the number of times to multiply
+ *	matrix1 - the first adjacency matrix
+ *	matrix2 - the second adjacency matrix
+ *	paths - the number of paths (times) to preform matrix multiplacation
+ *	count - the height of the matrix
+ */
+void CPUMultiplyMatrix(long** matrix1, long** matrix2, int paths, int count){
+	long* newMatrix = (long*) malloc(sizeof(long)*count*count);
+	int i, j, k;
+	while(paths > 0) {
+
+		for(i=0; i<count; i++) {
+			for(j=0; j<count; j++) {
+				for(k=0; k<count; k++) {
+					newMatrix[(i*count)+j] += (*matrix1)[(i*count)+k] * (*matrix2)[(k*count)+j];
+				}
+			}
+		}
+		//Copy newMatrix to matrix2 and clear newMatrix
+		for(i = 0; i < count*count; i++) {
+			(*matrix2)[i] = newMatrix[i];
+			newMatrix[i] = 0;
+		}
+		paths--;
+	}
+	free(newMatrix);
+}
+
+/*
+ * GPUMultiplyMatrix (GPU Only)- Returns a cross multiplied matrix of two matrixies (The matrixies must be of the same height and width)
+ * Each core is calculating an element on the multiplied matrix
+ * e.g.
+ *		
+ *		a b c		1a 1b 1c		2a 2b 2c
+ * 		d e f	=	1d 1e 1f	X	2d 2e 2f
+ * 		g h i		1g 1h 1i		2g 2h 2i
+ *		
+ * 	There will be 9 cores (3x3)
+ * 	Core 0, the first core will calculate (a) in the final matrix (1a*2a + 1d*2b + 1g*2c)
+ * 	Core 1, the first core will calculate (b) in the final matrix (1b*2a + 1e*2b + 1h*2c)
+ * 	And so on...
+ * 
+ * matrix1 - the first adjacency matrix
+ * matrix2 - the second adjacency matrix
+ * paths - the number of paths (times) to preform matrix multiplication
+ * count - the height of the matrix
+ */
+__global__ void GPUMultiplyMatrix(long* matrix1, long* matrix2, int paths, int count){
+	int element = blockIdx.x * blockDim.x + threadIdx.x;
+	int i;
+	while(paths > 0) {
+		long sum = 0;
+		int col = element % count;
+		int row = element / count;
+		for(i=0; i < count; i++) {
+			sum += matrix1[count*i + col] * matrix2[row*count + i];
+		}
+		//Wait till all GPU cores are finished
+		__syncthreads();
+		matrix2[element] = sum;	
+
+		paths--;
+	}
+}
+
+/*
+ * traverse (GPU Only) - takes in a matrix and returns all paths as an int array the
+ *	matrix - The original matrix
+ *	paths - An array storing the set of paths
+ *	matrix - The multiplied matrix
+ * 	count - the height of the matrixies
+ *	startNodeNumber - the starting node number (from 0 to count-1)
+ *	endNodeNumber - the ending node number (from 0 to count-1)
+ */
+__global__ void traverse(long* matrix, long* paths, int count, int startNodeNumber, int endNodeNumber, int length, int numPaths) {
+	int element = blockIdx.x * blockDim.x + threadIdx.x;
 
 	//curand = cuda random for random number generation
 	curandState state;
@@ -109,17 +152,17 @@ __global__ void traverse(int* matrix, int* paths, int count, int start, int end,
 	//current length of the path
 	int currLength = 0;
 	//current Node in the graph
-	int currNode = start;
-	//start is always the first Node
+	int currNode = startNodeNumber;
+
 	paths[element*length + currLength] = currNode;
 	currLength++;
-	while(currLength != length){
-		if(currLength == length-1){
+	while(currLength != length) {
+		if(currLength == length-1) {
 			//this case is to assist in our bruteforce algorithm
-			//if we can only make one more transition instead of doing 
-			//a random transition we try to move to the end point
-			if(matrix[currNode * count + end] == 1){
-				currNode = end;
+			//if we can only make one more transition instead of doing
+			//a random transition we try to move to the endNodeNumber point
+			if(matrix[currNode * count + endNodeNumber] == 1) {
+				currNode = endNodeNumber;
 				paths[element*length + currLength] = currNode;
 				currLength++;
 
@@ -128,206 +171,237 @@ __global__ void traverse(int* matrix, int* paths, int count, int start, int end,
 				//for(i = 0; i < numPaths; i++){
 				//	int j;
 				//	for(j = 0; j < length; j++){
-				//		
+				//
 				//	}
 				//}
-			}else{//if we can't connect to the endpoint we restart
+			} else {//if we can't connect to the endpoint we restart
 				currLength = 1;
-				currNode = start;
+				currNode = startNodeNumber;
 				paths[element*length + 0] = currNode;
-			}	
-		}else{
+			}
+		} else {
 			int randIdx;
-			do{
+			do {
 				randIdx = curand(&state) % count;
-			}while(matrix[currNode * count + randIdx] != 1);
-		        currNode = randIdx;
+			} while(matrix[currNode * count + randIdx] != 1);
+			currNode = randIdx;
 			paths[element*length + currLength] = currNode;
-        		currLength++;
+        	currLength++;
 		}
-	}
-
-}
-__global__ void multiply(int* matrixA, int* multipliedMatrix, int paths, int count){
-        int element = blockIdx.x*blockDim.x + threadIdx.x;
-	int i, j;
-	for(i=0; i < paths; i++)
-	{
-		int sum = 0;
-		int col = element % count;
-		int row = element / count;
-		for(j=0; j < count; j++){
-			sum+=matrixA[count*j + col]*multipliedMatrix[row*count + j];
-		}
-		__syncthreads();
-		multipliedMatrix[element] = sum;
 	}
 }
 
-//CPU matrix multiplication function
-void CPUMatrixMultiplication(int count, int path, int* matrix){
+/*
+ * CPUMatrixMultiplication - Preforms CPU matrix multiplication on an adjacency matrix
+ *	count - number of nodes
+ *	path - number of paths
+ *	matrix - an adjancency matrix
+ */
+void CPUMatrixMultiplication(int count, int path, long* matrix){
 	
+	//Array to store the multiplied array 
+	long* cpuMM = (long*)malloc(count*count*sizeof(long));
+
+	//Copy matrix to cpuMM
+	int i;
+	for(i = 0; i < count*count; i++) {
+		cpuMM[i] = matrix[i];
+	}
+
 	//Create the time interval
 	struct timeval start, end;
 
 	//Start time
 	gettimeofday(&start, NULL);
-	
+
 	//The completed multiplied matrix
-	int* multipliedMatrix =  multiplyMatrix(matrix, matrix, path, count);	
-	
+	CPUMultiplyMatrix(&matrix, &cpuMM, path, count);
+
 	//End time
 	gettimeofday(&end, NULL);
+
+	//Save the computed time
 	long microseconds = end.tv_usec - start.tv_usec;
-	
+
 	//Print the multiplied matrix
 	printf("CPU Generated matrix:\n");
-	if (!fTime){
-		printAdjMatrix(count, multipliedMatrix);
-	}
+	if (!fTimeOnly)
+		printAdjMatrix(count, cpuMM);
 	printf("Took %li microseconds to compute\n\n", microseconds);
-
+	
+	free(cpuMM);
 }
-//GPU matrix multiplication function
-void GPUMatrixMultiplication(int count, int path, int* matrix, int nodeA, int nodeB){
+/*
+ * GPUMatrixMultiplication - Preforms GPU matrix multiplication on an adjacency matrix
+ *	count - number of nodes
+ *	path - number of paths
+ *	matrix - an adjancency matrix
+ */
+void GPUMatrixMultiplication(int count, int path, long* matrix, int nodeA, int nodeB){
 
-	int numThreads = NUMTHREADS;
-		
-	//number at index of start*count + end this gets the total number of
-	//paths that exist	
+	//paths that exist
 	int numPaths;
 
 	//An adjacency matrix on the GPU
-	int* gpuMatrix;
+	long* gpuMatrix;
 
 	//The multiplied matrix on the GPU
-	int* gpuMM;
+	long* gpuMM;
 
 	//A matrix that will store gpuMM on the CPU
-	int* multipliedMatrix = (int*)malloc(count*count*sizeof(int));
+	long* multipliedMatrix = (long*)malloc(count*count*sizeof(long));
 
-	//The number of GPUS
-	int numBlocks = (count*count)/numThreads + 1;
+	//The number of GPUS needed for matrix multiplcation
+	int numBlocks = (count*count)/NUMTHREADS + 1;
 
 	//Allocate the memory on the GPU
-        cudaMalloc(&gpuMatrix, (count*count*sizeof(int)));
-	cudaMalloc(&gpuMM, (count*count*sizeof(int)));
+    cudaMalloc(&gpuMatrix, (count*count*sizeof(long)));
+	cudaMalloc(&gpuMM, (count*count*sizeof(long)));
 
-	//Create the time interval
+	//Copy the input matrix from the CPU to the GPU (matrix -> gpuMatrix)
+	cudaMemcpy(gpuMatrix, matrix, (count*count*sizeof(long)), cudaMemcpyHostToDevice);
+	cudaMemcpy(gpuMM, matrix, (count*count*sizeof(long)), cudaMemcpyHostToDevice);
+
+	//Create the time intervals
 	struct timeval start, end;
 
 	//Start time
 	gettimeofday(&start, NULL);
 
-	//Copy the input matrix from the CPU to the GPU (matrix -> gpuMatrix)
-        cudaMemcpy(gpuMatrix, matrix, (count*count*sizeof(int)), cudaMemcpyHostToDevice);
-        cudaMemcpy(gpuMM, matrix, (count*count*sizeof(int)), cudaMemcpyHostToDevice);
-	
-	//Preform the multiplied matrix function on gpuMatrix and store into gpuMM
-	multiply<<<numBlocks, numThreads>>>(gpuMatrix, gpuMM, path, count);
-	
+	//Preform the multiplied matrix function on gpuMatrix, and gpuMM, and store into gpuMM
+	GPUMultiplyMatrix<<<numBlocks, NUMTHREADS>>>(gpuMatrix, gpuMM, path, count);
+
+	//End time
+	gettimeofday(&end, NULL);
+
 	//Copy gpuMM from the GPU to the CPU in multipiedMatrix
-	cudaMemcpy(multipliedMatrix, gpuMM, (count*count*sizeof(int)), cudaMemcpyDeviceToHost);
+	cudaMemcpy(multipliedMatrix, gpuMM, (count*count*sizeof(long)), cudaMemcpyDeviceToHost);
+	cudaFree(&gpuMM);
+
+	//Calculate time
+	long microseconds = end.tv_usec - start.tv_usec;
+
+	//Print the multiplied matrix, copied earlier from the GPU
+	printf("GPU Generated matrix:\n");
+	if (!fTimeOnly)
+		printAdjMatrix(count, multipliedMatrix);
+	printf("Took %li microseconds to compute\n", microseconds);
+	printf("\n");
 	
-	cudaFree(&gpuMM);	
-
-        gettimeofday(&end, NULL);
-        long microseconds = end.tv_usec - start.tv_usec;
-
-        //Print the multiplied matrix, copied earlier from the GPU
-        printf("GPU Generated matrix:\n");
-        if (!fTime){
-                printAdjMatrix(count, multipliedMatrix);
-        }
-        printf("Took %li microseconds to compute\n", microseconds);
-	printf("\n");	
-	//gets num paths and if no paths exists it shows that and exits
+	//Get the number of paths
 	numPaths = multipliedMatrix[(nodeA * count) + nodeB];
-	if (numPaths == 0){
-		printf("No paths exist from %d to %d\n", nodeA, nodeB);
+	
+	//Print the paths
+	if (numPaths == 0) {
+		printf("No paths exist from Node<%d> to Node<%d>\n", nodeA, nodeB);
 		return;
-	}else{
+	} else {
 		path+=2;
-		numBlocks = numPaths / numThreads + 1;
-		int* paths = (int *)malloc(numPaths * sizeof(int) * (path));
-		int* gpuPaths;
-		cudaMalloc(&gpuPaths, (numPaths*path*sizeof(int)));
-		traverse<<<numBlocks, numThreads>>>(gpuMatrix, gpuPaths, count, nodeA, nodeB, path, numPaths);
-		cudaMemcpy(paths, gpuPaths, (numPaths*(path)*sizeof(int)), cudaMemcpyDeviceToHost);
+		numBlocks = numPaths / NUMTHREADS + 1;
+		long* paths = (long *)malloc(numPaths * sizeof(long) * (path));
+		long* gpuPaths;
+
+		cudaMalloc(&gpuPaths, (numPaths*path*sizeof(long)));
+		traverse<<<numBlocks, NUMTHREADS>>>(gpuMatrix, gpuPaths, count, nodeA, nodeB, path, numPaths);
+		cudaMemcpy(paths, gpuPaths, (numPaths*(path)*sizeof(long)), cudaMemcpyDeviceToHost);
 
 		cudaFree(&gpuMM);
 		int i;
-		for(i = 0; i < numPaths; i++){
-			int j;
-			for(j = 0; j < path; j++){
-				printf("%d ", paths[i*path + j]);
+		if (fShowPaths) {
+			for(i = 0; i < numPaths; i++) {
+				int j;
+				for(j = 0; j < path; j++) {
+					printf("%ld ", paths[i*path + j]);
+				}
+				printf("\n");
 			}
-			printf("\n");		
 		}
-	printf("There are %d paths\n", numPaths);
+	printf("From Node<%d> to Node<%d>: There are %d paths\n", nodeA, nodeB, numPaths);
 	}
 }
 
-//Creates an adjacency matrix
-//	count - the size of the matrix. the size is count X count)
-//	matrix - a pointer to an adjacency Matrix
-int* generateAdjMatrix(int count, int* matrix){
-	matrix = (int *)malloc(count*count*sizeof(int));
-	int i, j;
+//Main function
+int main(int argc, char* argv[]){
+	char usageString[500] = ("Usage:\n-t: Print the Calculation Time only\n-d: Default - Set number of nodes to 10, number of paths to 3\n");
+	strcat(usageString,"-g: Preform calculations on GPU only\n-s: Show the paths\n-c <num of nodes>\n-p <num of paths>\n");
+	strcat(usageString,"-a <start node number (0 to c-1)>\n-b <end node number (0 to c-1)>\n");
 
-	//Set the random seed to the current time
-	srand(time(NULL));
+	int count; 					//Number of nodes
+	int path;					//Number of paths
+	long* adjMatrix;
 
-	//Create a random adjacency matrix using rand
-	for (i = 0; i < count; i++){
-		for(j = 0; j < count; j++){
-			if(i != j){
-				int randomResult = rand() % 2;
-				matrix[(i *count) + j] = randomResult;
-				matrix[(j *count) + i] = randomResult;
-			}
+	//start and end of the path
+	int startNodeNumber = 0;
+	int endNodeNumber = 3;
+
+	//If there is more than 2 parameters
+	opterr = 0;
+	int c;
+
+	//If no parametters are passed
+	if(argc == 1) {
+		fprintf(stderr, "%s", usageString);
+		return 1;
+	}
+
+	while((c = getopt(argc, argv, "dgtsc:p:a:b:")) != -1) {
+		switch (c) {
+			//Flags
+			case 'd':
+				count = 10;
+				path = 2;
+				break;
+			case 'g':
+				fGPUOnly = 1;
+				break;
+			case 't':
+				fTimeOnly = 1;
+				break;
+			case 's':
+				fShowPaths = 1;
+			//Parameters
+			case 'c':
+				count = atoi(optarg);
+				break;
+			case 'p':
+				path = atoi(optarg);
+				break;
+			case 'a':
+				startNodeNumber = atoi(optarg);
+                break;
+			case 'b':
+				endNodeNumber = atoi(optarg);
+				break;
+			case '?':
+				if (optopt == 'c' || optopt == 'p' || optopt == 'a' || optopt == 'b') {
+					fprintf (stderr, "Option (-%c) requires an argument.\n\n%s\n", optopt, usageString);
+				} else {
+					fprintf(stderr, "%s", usageString);
+				}
+				return 1;
+			default:
+				printf("Error...");
+				return 2;
 		}
 	}
-	return matrix;
-}
 
-//Returns a cross multiplied matrix of two matrixies
-//	in - the first matrix
-//	in2 - the second matrix
-//	num - the number of times we do the multiplacation
-//	size -
-int* multiplyMatrix(int* in,int* in2,int num, int count){
-	if(num==0)
-		return in2;
-	int arr[count];
-	int i,j,k;
-	int z,n=0;
-	int* out = (int *) malloc(sizeof(int)*count*count);
-	
-	for(i=0; i<count; i++){
-		for(j=0; j<count; j++){
-			for(k=0;k<count;k++){
-				arr[k] = in[(i*count)+k] * in2[(k*count)+j];
-			}
-			for(z=0;z<count;z++){
-				n+=arr[z];	
-			}
-			out[(i*count)+j] = n;
-			n=0;
-		}
-	}
-	return multiplyMatrix(in,out,num-1,count);
-}
+	path--;
 
-//Prints the adjacency matrix to stdout
-void printAdjMatrix(int count, int* matrix){
-	int i;
-	for (i = 0; i < count; i++){
-		int j;
-		for (j = 0; j < count; j++){
-			printf("%3i ", matrix[(i * count) + j]);
-		} 
+	//adjMatrix now equals a new Random adjancency  Matrix
+	adjMatrix = generateAdjMatrix(count);
+
+	//Print the generated adjancency matrix
+	if (!fTimeOnly){
+		printf("Generated Adjancency Matritx:\n");
+		printAdjMatrix(count, adjMatrix);
 		printf("\n");
 	}
-}
 
+	//Compute the CPU function
+	if(!fGPUOnly)
+		CPUMatrixMultiplication(count, path, adjMatrix);
+
+	//Compute the GPU function
+	GPUMatrixMultiplication(count, path, adjMatrix, startNodeNumber, endNodeNumber);
+	return 0;
+}
